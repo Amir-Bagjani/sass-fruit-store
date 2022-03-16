460c2314ea87094072b710a3ce32a851e5f4ae08
amirbagjni <amirbagjani@gmail.com>
2022-03-16 22:48:59 +0330

    navbar-menu and login added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../style/navbar.scss";
 
 
 const Navbar = () => {
   const [open, setOpen] = useState("")
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=>{setOpen("")});
+
+    return () => window.removeEventListener('scroll')
+  }, [])
 
 
   return (
@@ -13,25 +19,71 @@ const Navbar = () => {
         <i className="fas fa-shopping-basket"></i> Fruit
       </a>
 
-      <nav className="navbar">
+      <nav className={open==="menu"?"navbar active":"navbar"}>
         <a href="#home">home</a>
-        <a href="#features">features</a>
-        <a href="#products">products</a>
-        <a href="#categories">categories</a>
-        <a href="#review">review</a>
-        <a href="#blogs">blogs</a>
+        <a href="#features">Features</a>
+        <a href="#products">Products</a>
+        <a href="#categories">Categories</a>
+        <a href="#review">Review</a>
+        <a href="#blogs">Blogs</a>
       </nav>
 
       <div className="icons">
-        <div className="fas fa-bars" id="menu-btn"></div>
+        <div className="fas fa-bars" id="menu-btn" onClick={()=>setOpen(pre=>pre==="menu"?"":"menu")}></div>
         <div className="fas fa-search" id="search-btn" onClick={()=>setOpen(pre=>pre==="search"?"":"search")}></div>
-        <div className="fas fa-shopping-cart" id="cart-btn"></div>
-        <div className="fas fa-user" id="login-btn"></div>
+        <div className="fas fa-shopping-cart" id="cart-btn" onClick={()=>setOpen(pre=>pre==="cart"?"":"cart")}></div>
+        <div className="fas fa-user" id="login-btn" onClick={()=>setOpen(pre=>pre==="login"?"":"login")}></div>
       </div>
 
       <form className={open==="search"?"search-form active":"search-form"}>
         <input type="search" id="search-box" placeholder="search here..." />
         <label htmlFor="search-box" className="fas fa-search"></label>
+      </form>
+
+      <div className={open==="cart"?"shopping-cart active":"shopping-cart"}>
+
+        <div className="box">
+          <i className="fas fa-trash"></i>
+          <img src="image/cart-img-1.png" alt="cart" />
+          <div className="content">
+            <h3>watermelon</h3>
+            <span className="price">$4.50/-</span>
+            <span className="quantity">qty : 1</span>
+          </div>
+        </div>
+
+        <div className="box">
+          <i className="fas fa-trash"></i>
+          <img src="image/cart-img-2.png" alt="cart" />
+          <div className="content">
+            <h3>onion</h3>
+            <span className="price">$4.50/-</span>
+            <span className="quantity">qty : 1</span>
+          </div>
+        </div>
+
+        <div className="box">
+          <i className="fas fa-trash"></i>
+          <img src="image/cart-img-3.png" alt="cart" />
+          <div className="content">
+            <h3>chicken</h3>
+            <span className="price">$4.50/-</span>
+            <span className="quantity">qty : 1</span>
+          </div>
+        </div>
+
+        <div className="total">total : $14.5/-</div>
+        <a href="#" className="btn">chekout</a>
+
+      </div>
+
+      <form className= {open==="login"?"login-form active":"login-form"}>
+        <h3>login now</h3>
+        <input type="email" className="box" placeholder="your email" />
+        <input type="password" className="box" placeholder="your password" />
+        <p>forgot your password <a href="#">click here</a></p>
+        <p>don't have an account <a href="#">create now</a></p>
+        <input type="submit" value="login now" className="btn" />
       </form>
 
     </header>
